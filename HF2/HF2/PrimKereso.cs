59bd85621a117107d5e78779bb2cf458b2c8ece9
markkoltai <markkoltai@gmail.com>
2017-02-21 18:37:45 +0100

    PrimeSearcher

---
+++ HF2/HF2/PrimKereso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF2
+{
+    class PrimKereso
+    {
+        public static bool PrimeSearcher(int szam)
+        {
+            bool prime = false;
+            int db = 0;
+            for (int i = 1; i <= szam; i++)
+            {
+                if (szam % i == 0)
+                {
+                    db++;
+                }
+            }
+            prime = db == 2;
+            return prime;
+        }
+    }
+}
